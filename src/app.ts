613cb91b23c5059a53087f64737f0a6556f13bb3
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-08-19 22:36:16 -0300

    feat: instalando fastify cors para permitir requisições via front end

--- src/app.ts
+++ src/app.ts
@@ -3,27 +3,35 @@ import { appRoutes } from "./http/controllers/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCors from "@fastify/cors";  // Importa o plugin CORS
 
-export const app = fastify()
+export const app = fastify();
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:3000', // Permite apenas essa origem
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+});
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET
-})
+});
 
-app.register(appRoutes)
+app.register(appRoutes);
 
 app.setErrorHandler((error, _request, reply) => {
-    if(error instanceof ZodError) {
+    if (error instanceof ZodError) {
         return reply
             .status(400)
-            .send({message: 'Validation error.', issues: error.format()})
+            .send({ message: 'Validation error.', issues: error.format() });
     }
 
-    if(env.NODE_ENV != 'production') {
-        console.error(error)
+    if (env.NODE_ENV !== 'production') {
+        console.error(error);
     } else {
-        //
+        // Exemplo: Enviar o erro para um serviço de monitoramento
+        // Sentry.captureException(error);
     }
 
-    return reply.status(500).send({message: 'Internal server error'})
-})
+    return reply.status(500).send({ message: 'Internal server error' });
+});
