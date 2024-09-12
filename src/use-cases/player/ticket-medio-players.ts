0678ba980a72325dbcdbf3a79cde1cbce6371247
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-09-12 18:06:56 -0300

    fix: pulando mÊs de abril

--- src/use-cases/player/ticket-medio-players.ts
+++ src/use-cases/player/ticket-medio-players.ts
@@ -31,31 +31,37 @@ export class GetTicketMedioUseCase {
         // Define o tamanho do lote para dividir as consultas
         const batchSize = 2; // Ajuste conforme necessário
         const monthBatches = [];
-        
+
         // Divide a lista de meses em lotes
         for (let i = 0; i < months.length; i += batchSize) {
             monthBatches.push(months.slice(i, i + batchSize));
         }
 
         const monthResults = [];
-        
+
         // Processa cada lote de meses
         for (const batch of monthBatches) {
             const batchPromises = batch.map(async (month, index) => {
+                // Pular o mês de abril
+                if (month === 'abril') {
+                    console.warn(`Pular processamento do mês ${month}`);
+                    return null; // Retorna null para meses que devem ser pulados
+                }
+
                 const monthIndex = months.indexOf(month);
                 const date_init = new Date(year, monthIndex, 1);
                 const date_finish = new Date(year, monthIndex + 1, 0); // Último dia do mês
-                
+
                 try {
                     const qtd_jogadores = await this.playersRepository.getQtdPlayersMonthByFtdDate(date_init, date_finish);
                     const totalAmount = await this.playersRepository.getTotalAmountMonthByFtdDate(date_init, date_finish);
-                    
+
                     if (typeof qtd_jogadores !== 'number' || typeof totalAmount !== 'number') {
                         throw new Error(`Dados inesperados: qtd_jogadores ou totalAmount não é um número.`);
                     }
-                    
+
                     const average = qtd_jogadores > 0 ? totalAmount / qtd_jogadores : 0;
-                    
+
                     return {
                         month,
                         result: {
@@ -71,7 +77,10 @@ export class GetTicketMedioUseCase {
             });
 
             // Aguarda a resolução de todas as promessas no lote
-            monthResults.push(await Promise.all(batchPromises));
+            const batchResults = await Promise.all(batchPromises);
+
+            // Filtra resultados válidos e adiciona ao array de resultados
+            monthResults.push(batchResults.filter(result => result !== null));
         }
 
         // Converte o array de resultados em um objeto
